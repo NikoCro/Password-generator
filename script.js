b54ac90e941264a7771f501dc6632b54da647a66
Nikola Lucic <nikola1983@yahoo.com>
2022-12-20 14:01:49 -0500

    added list of potential password charactercs

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-// Assignment Code
+
+var passLength = 12;
+var lowerCase =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var upperCase=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var numbers =["0","1","2","3","4","5","6","7","8","9"]
+var specChar =["!","@","#","$","%","^","&","?","+","-"]
+
+// assignment code
 var generateBtn = document.querySelector("#generate");
+,
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 
 // Write password to the #password input
 function writePassword() {
@@ -10,5 +22,22 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword()
+//this function is to prompt for user password info
+function userPrompts(){
+var passLength = parseInt(prompt("How long you want your password to be?"))
+if (passLength<8 || passLength >128) {
+    alert("Your number must be between 8 and 128")
+    return false
+    
+}
+
+
+}
+
+//this function is to take user's info from user prompts and to proccess the password
+
+    var userInfo= userPrompts()
+}
+/
+
